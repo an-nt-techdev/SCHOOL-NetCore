9b7846d650e37223bc2866dd8266b9feb6ed76ba
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2019-12-16 16:23:47 +0700

    ahihi

---
+++ ASP.NET_CORE_Final2019/ASP.NET_CORE_Final2019/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_CORE_Final2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
