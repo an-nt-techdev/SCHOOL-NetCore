7ee4f6b70503d622d81777729931015b45257b8e
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2019-12-17 16:09:30 +0700

    fix single product

--- ASP.NET_CORE_Final2019/ASP.NET_CORE_Final2019/Controllers/ShopController.cs
+++ ASP.NET_CORE_Final2019/ASP.NET_CORE_Final2019/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.NET_CORE_Final2019.Models;
 using ASP.NET_CORE_Final2019.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,17 +11,22 @@ namespace ASP.NET_CORE_Final2019.Controllers
     public class ShopController : Controller
     {
         public readonly IFSanpham _Sanpham;
+      
         public ShopController(IFSanpham _IFSanpham)
         {
             _Sanpham = _IFSanpham;
         }
         public IActionResult Index()
         {
+            ViewBag.ListLoaiSanPham = _Sanpham.GetLoaisanphams;
             return View(_Sanpham.GetSanPhams);
         }
         public IActionResult SingleProduct(int Id)
         {
-            return View(_Sanpham.GetChitietsanpham(Id));
+            ViewBag.Sanpham = _Sanpham.GetSanpham(Id);
+            ViewBag.Chitietsanpham = _Sanpham.GetChitietsanpham(Id);
+            ViewBag.Loai = _Sanpham.GetLoaisanpham(Id);
+            return View();
         }
     }
 }
