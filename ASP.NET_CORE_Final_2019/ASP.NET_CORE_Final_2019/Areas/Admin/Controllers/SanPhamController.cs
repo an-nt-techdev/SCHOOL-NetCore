73fb7ad70ccea44da38ec39ec2974b0a23e32fd8
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2019-12-18 22:43:59 +0700

    đang làm chọn ảnh

--- ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/SanPhamController.cs
+++ ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/SanPhamController.cs
@@ -10,21 +10,47 @@ using Microsoft.AspNetCore.Mvc;
 namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Route("admin/[controller]")]
     public class SanPhamController : Controller
     {
         public readonly IFSanpham IFSanpham;
         public readonly INhaCungCap INhaCungCap;
-        public SanPhamController(IFSanpham _IFSanpham,INhaCungCap _INhaCungCap)
+        public readonly ILoaiSanPham ILoaiSanPham;
+        public SanPhamController(IFSanpham _IFSanpham,INhaCungCap _INhaCungCap,ILoaiSanPham _ILoaiSanPham)
         {
             IFSanpham = _IFSanpham;
             INhaCungCap = _INhaCungCap;
+            ILoaiSanPham = _ILoaiSanPham;
         }
+        [Route("admin/[controller]")]
         public IActionResult Index()
         {
             ViewBag.ListNhaCungCap = INhaCungCap.GetNhacungcaps;
             ViewBag.ListLoaiSanPham = IFSanpham.GetLoaiSanPhams;
             return View(IFSanpham.GetSanPhams);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.ListLoaiSanPham = ILoaiSanPham.GetLoaisanphams;
+            ViewBag.ListNhaCungCap = INhaCungCap.GetNhacungcaps;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Sanpham _SanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                IFSanpham.addSanPham(_SanPham);
+                return RedirectToAction("Index");
+            }
+            return View(_SanPham);
+        }
+
+        public IActionResult Detail(int? Id)
+        {
+            return View();
         }
     }
 }
