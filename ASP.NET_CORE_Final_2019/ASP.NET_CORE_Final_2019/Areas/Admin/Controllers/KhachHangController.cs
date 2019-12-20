b16fff36c51f43e5863facceba3b61038f954f81
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2019-12-21 00:05:36 +0700

    fix

--- ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/KhachHangController.cs
+++ ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/KhachHangController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ASP.NET_CORE_Final_2019.Areas.Services;
+using ASP.NET_CORE_Final_2019.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
@@ -11,15 +12,29 @@ namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
     public class KhachHangController : Controller
     {
         public readonly IKhachHang IKhachHang;
-
-        public KhachHangController(IKhachHang _IKhachHang)
+        public readonly IDonHang IDonHang;
+        public KhachHangController(IKhachHang _IKhachHang,IDonHang _IDonHang)
         {
             IKhachHang = _IKhachHang;
+            IDonHang = _IDonHang;
         }
         [Route("admin/[controller]")]
         public IActionResult Index()
         {
+            ViewBag.DonHang = IDonHang.GetDonhangs;
             return View(IKhachHang.GetKhachhangs);
+        }
+        [HttpGet]
+        public IActionResult Edit(string Email)
+        {
+            return View(IKhachHang.GetKhachHang(Email));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Khachhang _KhachHang)
+        {
+            IKhachHang.UpdateKhachHang(_KhachHang);
+            return RedirectToAction("Index");
         }
     }
 }
