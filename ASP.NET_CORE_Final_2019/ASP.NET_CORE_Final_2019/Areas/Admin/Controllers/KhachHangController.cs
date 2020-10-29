6f03b41f5860d5a449fc6edbe170a24fe70f7dcc
Khoa <ohwhynotme1999@gmail.com>
2020-10-29 15:57:41 +0700

    add export as CSV or XLSX

--- ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/KhachHangController.cs
+++ ASP.NET_CORE_Final_2019/ASP.NET_CORE_Final_2019/Areas/Admin/Controllers/KhachHangController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ASP.NET_CORE_Final_2019.Areas.Services;
 using ASP.NET_CORE_Final_2019.Models;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
@@ -46,6 +48,26 @@ namespace ASP.NET_CORE_Final_2019.Areas.Admin.Controllers
                 builder.AppendLine($"{item.Email}");
             }
             return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "DanhSachEmailKhachHang.csv");
+        }
+        public IActionResult ExportXlsx()
+        {
+            IEnumerable<Khachhang> listemail = IKhachHang.GetKhachhangs;
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Emails");
+                var row = 1;
+                foreach (var item in listemail)
+                {
+                    worksheet.Cell(row,1).Value = item.Email;
+                    row++;
+                }
+                using( var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "DanhSachEmailKhachHang.xlsx");
+                }
+            }
         }
     }
 }
